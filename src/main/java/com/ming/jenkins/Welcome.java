dfcd8b67bb9b8f8b7df7a1c5602596f613b2c11c
I-Ming <soakpenny@hotmail.com>
2023-04-25 17:08:54 +0800

    update index page

--- src/main/java/com/ming/jenkins/Welcome.java
+++ src/main/java/com/ming/jenkins/Welcome.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Welcome {
 
-	@GetMapping(path = "/welcome")
+	@GetMapping(path = "/")
 	public String welcome(Model model) {
 		model.addAttribute("value", "test jenkins");
 		return "welcome";
